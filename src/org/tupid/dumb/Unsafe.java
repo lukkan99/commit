3560050d59b7a37a364ab356fc2106cbb28d2b24
Wilee999 <Infinitywilee@rocketmail.com>
2013-12-29 11:11:12 -0800

    ooh

--- src/org/tupid/dumb/Unsafe.java
+++ src/org/tupid/dumb/Unsafe.java
@@ -2,7 +2,14 @@ package org.tupid.dumb;
 
 public class Unsafe
 {
+    private static final Unsafe INSTANCE = new Unsafe();
+    
     public Unsafe()
     {
+    }
+    
+    public static Unsafe getUnsafe()
+    {
+        return Unsafe.INSTANCE;
     }
 }
